7bef74d6c9bbb85bdffbab19eba4ee6a6974894f
cju100 <shreeju.joshi000@gmail.com>
2022-06-30 11:38:20 +0545

    View Registred user by admin

--- sahayogi_front/src/components/home.js
+++ sahayogi_front/src/components/home.js
@@ -10,14 +10,10 @@ import Navbar from './navbar'
 export default function Home() {
   return (
     <div>
+    
 
         <div class="container-xxl bg-white p-0">
-        
-        {/* <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-            <div class="spinner-border text-primary"    role="status">
-                <span class="sr-only">Loading...</span>
-            </div>
-        </div> */}
+    
         
 
          {/* Navbar & Hero Start */}
